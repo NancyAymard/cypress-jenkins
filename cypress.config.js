c78c1eb4546283159f676aff85147ec1c2a97bf6
Nancy Noelia Aymard Lombardo <nancy.aymard@darwoft.com>
2023-12-18 11:27:53 -0300

    reports were added

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  video: false,
+  reporterOptions: {
+    charts: true,
+    reportTitle: "Cypress with Jenkins pipelines",
+    reportFilename: "my_report",
+    reportDir: "reports",
+    html: true,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
